fc52ce8e470245b53ca2ba2b02e14d6c46e96200
Raj Sumra <raj.sumratech@gmail.com>
2016-11-12 22:05:46 -0500

    updated the bot to speak

--- index.js
+++ index.js
@@ -29,4 +29,39 @@ app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
 
+app.post('/webhook/', function (req, res) {
+	messaging_events=req.body.entry[0].messaging
+	for (i=0; i<messaging_events.length; i++) {
+	    event=req.body.entry[0].messaging[i]
+	    sender=event.sender.id
+	    if (event.message && event.message.text) {
+		text=event.message.text
+		sendTextMessage(sender, "Text recieved, echo: " + text.substring(0,200))
+		}
+	     }
+	     res.sendStatus(200)
+	})
+
+	var token="EAAS0qe2E48IBAB08kKr9ZAEjZACGBBJK6XXAbiFchqmZBmBsZCKigKHSPHjU6a6ghLuZA2JiWHIB98n3bYV4xMcWeQHyGVb5fnfQQdf2ZABl7Ink4HZC2F7MXKK4LeFXWERM5Q6p8jNWtlGbGlEmAxjZCEUFUO1CqCy1qEutM99fLgZDZD"
+
+function sendTextMessage(sender, text) {
+	messageData={
+		text:"Hi, what's your name?"
+	}
+	request({
+	    url:'https://graph.facebook.com/v2.6/me/messages',
+	     qs:{access_token:token},
+	     method: 'POST',
+	     json: {
+		recipient: {id:sender},
+		message:messageData,
+		}
+	      }, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if(response.body.error) {
+			console.log('Error: ', repsonse.body.error)
+		}
+	    })
+	}
 
